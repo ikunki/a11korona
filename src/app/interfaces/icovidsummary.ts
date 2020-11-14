96b75485ee3ef57c0d6d7ace0c4acc088432e954
Kun Istvan <ikunki@outlook.com>
2020-11-14 19:39:57 +0000

    added interface ICountryData

--- src/app/interfaces/icovidsummary.ts
+++ src/app/interfaces/icovidsummary.ts
@@ -1,4 +1,17 @@
 
+export interface ICountryData {
+    Country: string,
+    CountryCode: string,
+    Date: string,
+    Slug: string,
+    NewConfirmed: number,
+    NewDeaths: number
+    NewRecovered: number,
+    TotalConfirmed: number,
+    TotalDeaths: number,
+    TotalRecovered: number
+}
+
 export interface IGlobal {
     NewConfirmed: number,
     NewDeaths: number,
@@ -11,5 +24,17 @@ export interface IGlobal {
 export interface ICovidSummary {
     Message: string,
     Global: IGlobal,
-    Countries: []
+    Countries: ICountryData[]
+}
+
+export interface ITotalMinConfirmed extends ICountryData {
+}
+
+export interface ITotalMinDeaths extends ICountryData {
+}
+
+export interface ITotalMaxConfirmed extends ICountryData {
+}
+
+export interface ITotalMaxDeaths extends ICountryData {
 }
